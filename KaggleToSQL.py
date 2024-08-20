d3e7e97440560a5724592697961ecbff818ff4a3
Shahood Sajid <shahood.sajid@hotmail.com>
2024-08-21 00:06:49 +0500

    ETL-Completed

--- KaggleToSQL.py
+++ KaggleToSQL.py
@@ -1,6 +1,7 @@
 import kaggle
 import zipfile
 import pandas as pd
+import sqlalchemy as sqla
 
 def extract(file):
     zip_ref = zipfile.ZipFile(file) 
@@ -15,15 +16,22 @@ def transformation(extracted_data):
     orders_df = pd.read_csv(extracted_data)
     null = orders_df.isnull().sum()
     #print(null)
-    orders_df.drop('Postal Code',axis = 1, inplace = True)
+    orders_df.drop(['Postal Code','Row ID'],axis = 1, inplace = True)
     orders_df.columns = orders_df.columns.str.lower()
     orders_df.columns = orders_df.columns.str.replace(' ','_')
     orders_df['order_date'] = pd.to_datetime(orders_df['order_date'], dayfirst=True)
     orders_df['ship_date'] = pd.to_datetime(orders_df['ship_date'], dayfirst=True)
-    print(orders_df.head(2))
     orders_df.to_csv('test.csv',index = False)
 
     return orders_df
+
+def load(sales_data):
+    engine = sqla.create_engine('mssql://JuicyLucy\MSSQLSERVER01/master?driver=ODBC+DRIVER+17+FOR+SQL+SERVER')
+    connection = engine.connect()
+    sales_data.to_sql('sales', con = connection , index=False, if_exists = 'append')
+    
     
 extracted_data = extract('train.csv.zip')
 sales_data = transformation(extracted_data)
+load(sales_data)
+
